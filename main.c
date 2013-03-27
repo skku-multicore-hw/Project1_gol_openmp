516a82fc888a9a5af292228956bef3f1513c484a
kbc8894 <kbc8894@gmail.com>
2013-03-27 16:34:49 +0900

    reset commit delete printf test, test2

--- main.c
+++ main.c
@@ -26,8 +26,6 @@ int main(int argc, char** argv){
 
 	fprintf(stderr,"Elapsed Time  : %ld(msec)\n",	1000*(result_time.tv_sec) + (result_time.tv_usec/1000));
 
-	printf("test");
-	printf("test2");
 
 	return 0;
 }
